0cccf44e4964aafc9a6bafcf0401019dc5f5f24b
edgar ludena <edgar.ludena90@gmail.com>
2022-12-15 08:12:54 -0500

    commit tree 176cfd13aa337815a7d1fdecc0ea49b44c543fd4

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,11 @@
-
-
+class Book
+  #these are instances methods line 4 and 5 
+  attr_accessor :author, :genre,:page_count
+  attr_reader :title
+  def initialize(title)
+    @title = title
+  end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
